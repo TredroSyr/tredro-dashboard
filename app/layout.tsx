ef67268becfa427187da2ed6146a4e98bf8bc8a0
safwatbilal <safwetbilal65@gmail.com>
2026-09-18 11:23:33 +0300

    feat: implement deep link handling and update app links for new domain

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { Toaster } from "@/components/ui/toast";
 import { QueryProvider } from "@/components/provider/QueryClientProvider";
 
 import HardwareBackButton from "@/components/tredro/HardwareBackButton";
+import DeepLinkHandler from "@/components/tredro/DeepLinkHandler";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -106,6 +107,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           <Suspense fallback={null}>
             <HardwareBackButton />
           </Suspense>
+          <DeepLinkHandler />
         </QueryProvider>
       </body>
     </html>
