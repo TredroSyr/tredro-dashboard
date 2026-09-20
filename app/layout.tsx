0e8c85d4d8864211ad06f94a7e67a44fdd518a6b
safwatbilal <safwetbilal65@gmail.com>
2026-09-20 23:04:43 +0300

    feat: replace Sonner with custom toast component and remove Sonner dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import AppDownloadDrawer from "@/layout/app-download-drawer";
 import { thmanyahSans } from "@/lib/fonts";
 
 import { Toaster } from "@/components/ui/toast";
-import { Toaster as SonnerToaster } from "sonner";
 import { QueryProvider } from "@/components/provider/QueryClientProvider";
 
 import HardwareBackButton from "@/components/tredro/HardwareBackButton";
@@ -104,7 +103,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           {children}
           <AppDownloadDrawer />
           <Toaster />
-          <SonnerToaster position="top-center" richColors />
           {/*  <ErudaLoader />*/}
           <Suspense fallback={null}>
             <HardwareBackButton />
